ffa268dd12cf01e41a9911f098f4f07cee88ded9
easyCore <easy@coresolutions.co.kr>
2019-07-15 16:32:03 +0900

    author 테이블 사용을 위해 objects/author.php 를 생성한다.
    readAll 메소드를 추가한다.

---
+++ board/ver_api/api/objects/author.php
@@ -0,0 +1,33 @@
+<?php
+class Author{
+
+    // database connection and table name
+    private $conn;
+    private $table_name = "author";
+
+    // object properties
+    public $id;
+    public $name;
+    public $profile;
+
+    public function __construct($db){
+        $this->conn = $db;
+    }
+
+    // used by select drop-down list
+    public function readAll(){
+        // select all data
+        $query = "SELECT
+                    id, name, profile
+                FROM $this->table_name
+                ORDER BY
+                    name";
+                    
+        $stmt = $this->conn->prepare($query);
+        $stmt->execute();
+
+        return $stmt;
+    }
+
+}
+?>
