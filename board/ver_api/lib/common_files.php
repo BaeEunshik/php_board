689458f97eeb911f6b90bb2ff784bc18d3f471e7
easyCore <easy@coresolutions.co.kr>
2019-07-16 09:56:56 +0900

    api 버전이 아닌 기존 소스를 삭제한다.

--- board/ver_api/lib/common_files.php
+++
@@ -1,8 +0,0 @@
-<?php
-    echo '<link rel="stylesheet" href="css/reset.css">';
-    echo '<link rel="stylesheet" href="css/common.css">';
-    echo '<script
-    src="https://code.jquery.com/jquery-3.4.1.js"
-    integrity="sha256-WpOohJOqMqqyKL9FccASB9O0KwACQJpFTUBLTYOVvVU="
-    crossorigin="anonymous"></script>';
-?>
