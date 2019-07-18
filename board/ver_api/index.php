51bcc9653022e12d9f9c273a23967a596b023a36
plantGrace <dmstlr888@gmail.com>
2019-07-18 17:25:30 +0900

    인덱스 페이지 생성

---
+++ board/ver_api/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/reset.css">
+    <link rel="stylesheet" href="css/common.css">
+    <script src="https://code.jquery.com/jquery-3.4.1.js" 
+    integrity="sha256-WpOohJOqMqqyKL9FccASB9O0KwACQJpFTUBLTYOVvVU="
+    crossorigin="anonymous">
+    </script>
+    <style>
+        .welcome{
+            margin-top:100px;
+            text-align:center;
+            font-size:30px;
+        }
+    </style>
+</head>
+<body>
+    <?php require("fragments/header.php") ?>
+    <h2 class="welcome">
+        WELCOME!
+    </h2>
+</body>
+</html>
